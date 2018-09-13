02ed4a8f1aa65682d80eae4fa074cc486eadd30e
vladglushko1006 <vladglushko1006@gmail.com>
2018-09-13 11:14:43 +0300

    Edit cancel button

--- app/src/main/java/com/vladglush/lab2/MainActivity.java
+++ app/src/main/java/com/vladglush/lab2/MainActivity.java
@@ -6,24 +6,24 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
 public class MainActivity extends Activity {
 
     EditText nameEdit;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nameEdit = findViewById(R.id.editTextName);
+        tv = findViewById(R.id.textViewHello);
     }
 
     public void onClickClear(View view) {
         nameEdit.setText("");
     }
-    
+
     public void onSayHelloButtonClick(View v) {
-        TextView tv = findViewById(R.id.textViewHello);
         String name = nameEdit.getText().toString().trim();
 
         if (!name.isEmpty())
